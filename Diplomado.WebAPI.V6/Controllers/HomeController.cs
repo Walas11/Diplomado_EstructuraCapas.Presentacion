2005e6c59a22d11dc7c22f17adf338681a38be95
walas <sebastianaceroleon@outlook.com>
2024-03-31 00:54:27 -0500

    feat: Se realiza cambio de Core, estaba en Core 8.0 y la pase a 6.0, luego se referencia de webApi.V6 a la capa de Dominio_EstructuraCapas.Comunes

---
+++ Diplomado.WebAPI.V6/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Diplomado_EstructuraCapas.Dominio.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Diplomado.WebAPI.V6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[Action]")]
+        public string ObtenerEstudiantes()
+        {
+            EstudiantesServices services = new EstudiantesServices();
+            return JsonConvert.SerializeObject(services.ObtenerEstudiantes());
+        }
+    }
+}
